027b58fd5c1bb2ad7b84e089f5f162da6d820d43
alaasifariny <33457908+alaasifariny@users.noreply.github.com>
2017-12-05 22:36:52 +0100

    jag ställer en fråga till databasen

--- uppgifter/Login/submit.php
+++ uppgifter/Login/submit.php
@@ -7,4 +7,5 @@ if(isset($_POST['username'])){
 $dbc = mysqli_connect("localhost","root","","register");
 
     
+  $query = "SELECT * FROM register WHERE username='".$_POST['username']."' and password='".$_POST['password']."'";
   
