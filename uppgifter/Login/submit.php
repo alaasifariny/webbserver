e7d547ef9051d5e70a9e9870e8203c4a150c3696
alaasifariny <33457908+alaasifariny@users.noreply.github.com>
2017-12-05 23:08:48 +0100

    om det funka...

--- uppgifter/Login/submit.php
+++ uppgifter/Login/submit.php
@@ -10,3 +10,4 @@ $dbc = mysqli_connect("localhost","root","","register");
   $query = "SELECT * FROM register WHERE username='".$_POST['username']."' and password='".$_POST['password']."'";
   
     $result = mysqli_query($dbc,$query);
+    if(mysqli_fetch_array($result)){
